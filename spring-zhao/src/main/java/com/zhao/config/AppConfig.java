c647d3907519af6037e35d12d16794bc968d033b
zhaojl131415 <zhaojl131415+>
2020-03-10 17:22:31 +0800

    BeanFactoryPostProcessor

--- spring-zhao/src/main/java/com/zhao/config/AppConfig.java
+++ spring-zhao/src/main/java/com/zhao/config/AppConfig.java
@@ -3,9 +3,12 @@ package com.zhao.config;
 import com.zhao.zhaoBatis.ZhaoMapper;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.core.type.filter.AnnotationTypeFilter;
+
+
 
 /**
  * @author zhaojinliang
@@ -16,7 +19,7 @@ import org.springframework.core.type.filter.AnnotationTypeFilter;
 // https://www.cnblogs.com/CodeBear/p/10336704.html
 //@Configurable
 @ComponentScan(basePackages ="com.zhao",
-		includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = ZhaoMapper.class))
+		includeFilters = @Filter(ZhaoMapper.class))
 @EnableAspectJAutoProxy
 public class AppConfig {
 }
